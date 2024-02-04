f1abbdbe3c2cfe2febac95141e7f017e50298056
Roman Nichi <Roman.Nichi.O@gmail.com>
2024-02-04 16:26:37 +0400

    use cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   await app.listen(process.env.PORT, () =>
     console.log(
       `Server run on ${process.env.PORT} in ${process.env.CONFIGURATION_MODE} mode`,
